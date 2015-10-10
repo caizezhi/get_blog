b53c5b2c05027455ebbd3373a2a4120be28f3b62
CAI <caizezhi@gmail.com>
2015-10-10 10:56:52 +0800

    更新查询获取作者代号

--- get_blog.py
+++ get_blog.py
@@ -55,28 +55,29 @@ def get_content():
         sj = soup.find(class_="xspace-smalltxt").get_text()
         sj1 = re.search(r"\d+-\d+-\d+\s\d+:\d+:\d+", str(sj))
         sj_now = "20"+sj1.group()
-        sj_json = json.dumps(sj1.group())
         sj_test = re.search(r"\d+-\d+-\d+", str(sj)).group()
         if sj_test != Nowaday:
-            conn = MySQLdb.connect(host='localhost', user='root', passwd='你的密码', db='你的数据库名', charset='utf8')
-            cursor = conn.cursor()
+            conn = MySQLdb.connect(host='localhost', user='root', passwd='你的密码', db='你的数据库', charset='utf8')
            # cursor.execute("""create database if not exists 你的数据库名""")
            # conn.select_db('你的数据库名')
            # cursor.execute("""create table if not exists cai_article(author int, `time` int, edittime int, eidtor char(100), `from` char(100), fromurl char(100), `type` int, thumb char(100), top int, click int, realclick int, title char(100), content text)""")
             num1 = random.randrange(100, 200)
-            value = (1, int_time(sj_now), int_time(Nowaday1), 'xxxx', 'xxxx', "http://uzone.univs.cn/blog/" + urls[c], 0, '0', 0, num1, 0, tm_json, zw_json)
+            cursor = conn.cursor()
+            cursor.execute("select uid from user where realname='测试账号'")
+            uid = re.search(r"\d+", str(cursor.fetchall())).group()
+            value = (int(uid), int_time(sj_now), int_time(Nowaday1), 'XXXX', 'XXXXXX', "http://uzone.univs.cn/blog/" + urls[c], 0, '0', 0, num1, 0, tm_json, zw_json)
             try:
-            	cursor.execute("select fromurl from article where fromurl=%s"%url_json)
-            	result = cursor.fetchall()
-            	if not result:
-                	print "not record"
+                cursor.execute("select fromurl from article where fromurl=%s"%url_json)
+                result = cursor.fetchall()
+                if not result:
+                    print "not record"
                 # Execute the SQL command
-                	cursor.execute("""insert into article(author, `time`, edittime, editor, `from`, fromurl, `type`, thumb, top, click, realclick, title, content) values(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s,%s, %s, %s)""", value)
+                    cursor.execute("""insert into article(author, `time`, edittime, editor, `from`, fromurl, `type`, thumb, top, click, realclick, title, content) values(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s,%s, %s, %s)""", value)
                 # Commit your changes in the database
-                	conn.commit()
-                	print "inserted"
-            	else:
-                	print "exist"
+                    conn.commit()
+                    print "inserted"
+                else:
+                    print "exist"
             except:
                  #Rollback in case there is any error
                 print "rollback"
@@ -85,12 +86,12 @@ def get_content():
 
 
 while 1:
-	hour = time.strftime('%H', time.localtime(time.time()))
-	sec = time.strftime('%S', time.localtime(time.time()))
-	min = time.strftime('%M', time.localtime(time.time()))
-	Nowtime = str(hour)+":"+str(min)+":"+str(sec)
-	#if min == 54:
-	#	print Nowtime
-	if Nowtime == "00:55:00":
-		print Nowtime
-		get_content()
+    hour = time.strftime('%H', time.localtime(time.time()))
+    sec = time.strftime('%S', time.localtime(time.time()))
+    min = time.strftime('%M', time.localtime(time.time()))
+    Nowtime = str(hour)+":"+str(min)+":"+str(sec)
+    #if min == 54:
+    #	print Nowtime
+    if Nowtime == "00:55:00":
+        print Nowtime
+        get_content()
